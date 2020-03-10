d21425d298a30ec8f0903513a19b3edb43c5a6e9
ibone <ibonerabert@gmail.com>
2020-03-10 17:28:45 +0100

    ejercicio 4

--- doc.java
+++ doc.java
@@ -10,15 +10,15 @@ public interface doc
 {
    
    // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
-   /** @param tiene dos parametro uno es la x es el contenido de filas
-    * @param file es el archivo
-    * @return el numero de filas y el archivo
+   /** 
+    * @param x es la filas y el file es la ruta absoluta
+    * @return el numero de filas y el file
     */
    public abstract String obtenerLinea(int x, String file);
    
    /**entrada:direccion es un String
     * precondicion:
-    * potscondicion:
+    * postcondicion:
     * salida: latidud y altitud los dos son un float
     */
    /** el metodo calcula la latitud y altitud de la direccion que has introducido
