2321f81ab41cd3aee96d09f2f5e4fe3e825af544
ibone <ibonerabert@gmail.com>
2020-03-10 16:39:58 +0100

    Intercambio inicial de proyecto

---
+++ doc.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Write a description of class doc here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface doc
+{
+   
+   // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
+   public abstract String obtenerLinea(int x, String file);
+   
+   
+}
---
+++ pruebas.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class pruebas here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class pruebas
+{
+    public static int paraProbar(int x)
+    {
+        if(x<4)
+        {
+            return x-4;
+        }
+        else
+        {
+            if(x%2==0)
+            {
+                return x + 2;
+            }
+            else
+            {
+                return x+1;
+            }
+        }
+    }    // instance variables - replace the example below with your own
+}
